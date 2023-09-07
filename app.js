f1cce490e848cdc780191c1e703740d1156bdc47
Anoush khan <143010760+Anoush455@users.noreply.github.com>
2023-09-07 17:25:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+// Chp#1 (ALERT)
+alert("First Name; Anoush")
+alert("Last Name; Khan")
+alert("Email;******")
+alert("Phone Number:******")
+alert("password********")
+
+
+
+// Chp#2 (Variable For String)
+let myCamelCaseVariable = "This is a CamelCase Variable"
+var myVariable; //declaration without definition
+myVariable = "this is a string value"; // Assigment with is a string value
+let TeamName = "My Cricket Team"
+alert("Other team Name is:")
+alert("my team Name")
+BestMan ="John"
+
+
+// chp#3 (Variables for numbers)
+var caseQty; //you can also use "const" or "var" depending on your needs
+var caseQty = 144;
+var num = 9;
+var result;
+result = 5 + 3;
+var merchtotal = 100;
+var shippingCharge = 10;
+var Ordertotal = merchtotal + shippingCharge;
+console.log(Ordertotal);
+var myVariable = 5;
+myVariable = myVariable + 10;
+console.log(myVariable); //output will be 15
+
+
+// chp#4(Variable names Legal and Illegal)
+var productCost = 3.45;
+var NameOfBrand
+var myNumber = 42;
+
+// chp#5(Math Expression I)
+var remainder = 10 % 3; //remainder will be 1
+var largeNum = 1000 * 2000;
+var Variable1 = 10;
+var Variable2 = 5;
+var result = Variable1 - Variable2;
+
+//prompt the user to enter the first number and conveert it to a float 
+var num1 = parseFloat(prompt("enter the first number"));
+
+//prompt the user to enter the Second number and conveert it to a float
+var num2 = parseFloat(prompt("enter the Second number"));
+
+// Calculate the result of the multiplication
+var result = num1 * num2
+
+// Display the result in a alert
+alert("The result of the multiplication is: " + result);
